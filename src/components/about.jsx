00bb85045b24c970e3938fabe1d3fbed7929a790
FERNAA90 <alex.fernandez_valladares@roche.com>
2022-01-30 23:27:14 +0100

    first approach

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,38 +1,22 @@
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export const About = (props) => {
   return (
     <div id='about'>
       <div className='container'>
         <div className='row'>
           <div className='col-xs-12 col-md-6'>
-            {' '}
-            <img src='img/about.jpg' className='img-responsive' alt='' />{' '}
-          </div>
-          <div className='col-xs-12 col-md-6'>
             <div className='about-text'>
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : 'loading...'}</p>
-              <h3>Why Choose Us?</h3>
-              <div className='list-style'>
-                <div className='col-lg-6 col-sm-6 col-xs-12'>
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : 'loading'}
-                  </ul>
-                </div>
-                <div className='col-lg-6 col-sm-6 col-xs-12'>
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : 'loading'}
-                  </ul>
-                </div>
-              </div>
+              <span class="shiny">
+                <span class="inner-shiny"> trailer</span>
+                
+              </span>
             </div>
+          </div>
+          <div className='col-xs-12 col-md-6'>
+            <iframe className='trailer-embed' src="https://www.youtube.com/embed/hvoD7ehZPcM" title="YouTube video player"></iframe>
           </div>
         </div>
       </div>
